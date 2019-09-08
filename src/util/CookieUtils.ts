0a7951d3594f21c64cbe6b396f4caaec7e9de9b5
c332030 <c332030@gmail.com>
2019-09-08 21:31:13 +0800

    配置文件相对路径，添加 redux 依赖

---
+++ src/util/CookieUtils.ts
@@ -0,0 +1,12 @@
+/**
+ * <p>
+ *   Description: CookieUtils
+ * </p>
+ * @author c332030（袁兴旺）
+ * @version 1.0
+ * @date 2019-8-7 19:45
+ */
+
+export class CookieUtils {
+
+}
--- src/util/index.ts
+++ src/util/index.ts
@@ -8,3 +8,4 @@
  */
 
 export * from './AxiosUtils'
+export * from './CookieUtils'
